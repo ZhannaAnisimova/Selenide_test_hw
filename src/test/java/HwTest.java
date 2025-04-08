be28c3901aba75220ee2094faa8405c04cd93e57
Zhanna Anisimova <zhzhv@yandex.ru>
2025-04-08 21:06:15 +0300

    add test

--- src/test/java/HwTest.java
+++ src/test/java/HwTest.java
@@ -1,20 +1,38 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 
 public class HwTest {
-    @BeforeAll
-    static void beforeAll() {
-    Configuration.browserSize = "1920x1080";
-    Configuration.baseUrl = "https://github.com/selenide/selenide";
-    Configuration.pageLoadStrategy = "eager";
+
+    @Test
+    void HomeWorkSelenide () {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-wrapper").shouldHave(text("Soft assertions"));
+        $(byText ("Soft assertions")).click();
+        $("#wiki-wrapper").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+        sleep(5000);
+
+    }
 }
 
-@Test
+
