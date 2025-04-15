c9e8b9010b519a677dc4f29ba08fdd93c0db692b
Zhanna Anisimova <zhzhv@yandex.ru>
2025-04-15 22:14:54 +0300

    add newtest

---
+++ src/test/java/githubTest.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Text;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class githubTest {
+    @Test
+    void githubTest () {
+        open ("https://github.com/");
+        
+    }
+}
